c80d172f7509e4e5791eaf87ff7b3163a1a7abdd
Daniel Bogdoll <daniel.bogdoll@saym.io>
2020-06-04 19:55:46 +0200

    2_16 done

---
+++ 1_Supervised/2_18 Linear Regression SciKit/gapminder1.py
@@ -0,0 +1,16 @@
+# TODO: Add import statements
+from sklearn.linear_model import LinearRegression
+
+
+# Assign the dataframe to this variable.
+# TODO: Load the data
+bmi_life_data = None 
+
+# Make and fit the linear regression model
+#TODO: Fit the model and Assign it to bmi_life_model
+bmi_life_model = LinearRegression()
+
+# Make a prediction using the model
+# TODO: Predict life expectancy for a BMI value of 21.07931
+bmi_life_model.fit(x,y)
+laos_life_exp = None
