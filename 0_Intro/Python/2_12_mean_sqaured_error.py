3c6fd7d2cdecbeb470f44b171b04f6f0c2173c0c
Daniel Bogdoll <daniel.bogdoll@saym.io>
2020-06-04 15:22:20 +0200

    Mean suqared done

--- 0_Intro/Python/2_12_mean_sqaured_error.py
+++ 0_Intro/Python/2_12_mean_sqaured_error.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Mean absolute error
+# Mean squared error
 # line: y = 1.2x + 2
 # line: y = w1x + w2
 # points: (2, -2), (5, 6), (-4, -4), (-7, 1), (8, 14)
@@ -8,7 +8,6 @@ import numpy as np
 line_w1 = 1.2
 line_w2 = 2
 points = np.array([[2, -2], [5, 6], [-4, -4], [-7, 1], [8, 14]])
-#numberOfPoints = points.shape[0] #shape is (5,2)
 numberOfPoints = len(points) 
 
 sumOfErrors = 0
@@ -17,10 +16,10 @@ for point in points:
     y_point = point[1]
     print (x_point , " " , y_point)
     y_line = line_w1 * x_point + line_w2
-    error = abs(y_point - y_line)
+    error = pow((y_point - y_line),2)
     print (error)
     sumOfErrors += error
 
 print ("Sum of errors ", sumOfErrors)
-meanAbsoluteError = 1/numberOfPoints * sumOfErrors
+meanAbsoluteError = 1/(2*numberOfPoints) * sumOfErrors
 print("Mean Absolute Error ", meanAbsoluteError)
