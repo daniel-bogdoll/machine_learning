c848521aea2c7af37631bb62edd414ea9ed541d4
Daniel Bogdoll <daniel.bogdoll@saym.io>
2020-06-10 21:30:33 +0200

    Finished project 1

--- 1_Supervised/10_Project_Finding Donors for CharityML/visuals.py
+++ 1_Supervised/10_Project_Finding Donors for CharityML/visuals.py
@@ -115,7 +115,7 @@ def evaluate(results, accuracy, f1):
     
     # Aesthetics
     pl.suptitle("Performance Metrics for Three Supervised Learning Models", fontsize = 16, y = 1.10)
-    pl.tight_layout()
+    pl.tight_layout(pad=5.0)
     pl.show()
     
 
@@ -139,5 +139,5 @@ def feature_plot(importances, X_train, y_train):
     pl.xlabel("Feature", fontsize = 12)
     
     pl.legend(loc = 'upper center')
-    pl.tight_layout()
+    pl.tight_layout(pad=5.0)
     pl.show()  
