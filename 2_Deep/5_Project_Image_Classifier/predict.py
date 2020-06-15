6a75d44fbf0258332cff2381605fa2ffd1864cf9
Daniel Bogdoll <daniel.bogdoll@saym.io>
2020-06-15 20:23:24 +0200

    Started part 2 of project

---
+++ 2_Deep/5_Project_Image_Classifier/predict.py
@@ -0,0 +1,3 @@
+#Usage:   python predict.py /path/to/image saved_model
+#Options: python predict.py /path/to/image saved_model --top_k
+#         python predict.py /path/to/image saved_model --category_names map.json
